ec39b7d2a9010841736b91eceb7e66c2f2189798
Oleksandr_Novak <asa.novak@cryptacash.company>
2024-02-23 11:16:04 +0200

    access control origin changed

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', 'https://dashboard.lttrbx.link');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
